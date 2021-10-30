cf68f80c6fb9b72f58d72f9ce85455035b29642e
mohsin-8 <mohsinalikhan518@gmail.com>
2021-10-30 22:32:29 +0500

    complete navbar

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import Components
+import Navbar from './Navbar/Navbar';
+
+function Home() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-around" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Tokenomics</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Whitepaper</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Our Technology</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Roadmap</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Team</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Faqs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ms-auto">
+                            <button class="btn btn-outline-secondary">JOIN PRE-SALE</button>
+                        </ul>
+                        <ul className="navbar-nav ms-auto">
+                            <button class="btn btn-outline-secondary">BUY TOKEN</button>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
