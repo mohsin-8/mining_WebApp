b588829fda8236de1f1c4bd82c4fd2c6b9c210a0
mohsin-8 <mohsinalikhan518@gmail.com>
2021-10-31 15:45:50 +0500

    changes in praragraph

--- src/components/Sections/About.js
+++ src/components/Sections/About.js
@@ -12,7 +12,7 @@ function About() {
                         <h4 style={{ color: '#f1a730' }} className="featurette-heading">
                             Mission
                         </h4>
-                        <p class="lead">To provide multiple utilities for DC coin holders and make it a native asset that is interoperable across the charity and the social media ecosystem.</p>
+                        <p class="lead">To provide multiple utilities for SHEEDA coin holders and make it a native asset that is interoperable across the charity and the social media ecosystem.</p>
                     </div>
                     <div class="col-md-5 my-4">
                         <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src={about} alt="" />
